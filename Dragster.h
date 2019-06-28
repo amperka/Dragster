27ff4733e5e104067ce96d6879d2323c247733c8
Yury Botov <yury.botov@gmail.com>
2019-06-28 17:15:55 +0300

    Prettyfy Dragster.h, add  banner

--- Dragster.h
+++ Dragster.h
@@ -1,24 +1,36 @@
+/*
+ * This file is a part of Dragster car set library.
+ *
+ * Defines: dragster object
+ * © Amperka LLC (https://amperka.com, dev@amperka.com)
+ * 
+ * Author: Harry Morgan <morgan@amperka.ru>
+ * Refactored by: Yury Botov (by@amperka.com)
+ * License: GPLv3, all text here must be included in any redistribution.
+ */
+
 
 #include <Arduino.h>
 
-#define SWAP_LEFT  0b00000010
+#define SWAP_LEFT 0b00000010
 #define SWAP_RIGHT 0b00000001
-#define SWAP_BOTH  0b00000011
+#define SWAP_BOTH 0b00000011
 
 class Dragster {
-  public:
+public:
     Dragster();
     void begin();
-	void begin(int direction);
+    void begin(int direction);
     void drive(int left, int right);
-	void driveF(float left, float right);
+    void driveF(float left, float right);
     void encodersBegin(void (*left)(), void (*right)());
     void leftEncoder(void (*left)(), int param);
     void rightEncoder(void (*left)(), int param);
     bool readButton();
-	void waitButton();
+    void waitButton();
     void led(int state);
-  private:
+
+private:
     byte swappedLeft = 0;
     byte swappedRight = 0;
 };
